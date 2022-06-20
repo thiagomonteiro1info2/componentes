19e1c5b5bf79a9e81d559ca1d3e619192a992e60
thiagomonteiro1info2 <99137961+thiagomonteiro1info2@users.noreply.github.com>
2022-06-20 08:16:28 -0300

    Add files via upload

---
+++ componentes/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import MenuLateral from "@/components/MenuLateral.vue";
+import CabecalhoCima from "@/components/CabecalhoCima.vue";
+import PrincipalContent from "@/components/PrincipalContent.vue";
+import RodapeFooter from "@/components/RodapeFooter.vue";
+
+export default {
+  components: { MenuLateral, CabecalhoCima, PrincipalContent, RodapeFooter },
+};
+</script>
+<template>
+  <MenuLateral />
+  <CabecalhoCima />
+  <PrincipalContent />
+  <RodapeFooter />
+</template>
+<style></style>
---
+++ componentes/src/components/articleBlock.vue
@@ -0,0 +1,8 @@
+<script>
+export default{
+  name: "articleBlock",
+};
+</script>
+<template>
+  <article class="block"></article>
+</template>
